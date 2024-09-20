a475c6e43937470f3ca0ec9f31911bbd744bc35e
Buzwe <buzwekabalaza02@gmail.com>
2024-09-20 22:31:07 +0200

    Calendar_2024

---
+++ calendar_2024.py
@@ -0,0 +1,10 @@
+import stdio
+import calendar
+
+year = 2024
+month = 8 #th
+x = calendar.month(year,month)
+stdio.writeln(x)
+stdio.writeln("This year was'nt easy but we'll never let go off the wheel")
+stdio.writeln("Done!")
+
